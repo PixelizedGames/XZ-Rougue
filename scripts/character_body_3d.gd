6ecdfed92072f4f523029a57a139eb4ddeca6a95
PixelizedGames <wf60@mahurangi.school.nz>
2025-06-23 09:48:28 +1200

    flickering lights

--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 var vertical_look_angle = 0.0
-var SPEED = 9.0
+var SPEED = 9
 var PSReady = true
+var SPEEDY = 0
 const JUMP_VELOCITY = 5.0
 const MAX_LOOK_ANGLE = 90.0
 const MIN_LOOK_ANGLE = -90.0
@@ -14,7 +15,7 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_pressed("shoot"):
 		_spawn_bullet()
 	if Input.is_action_pressed("ctrl"):
 		SPEED = 2
@@ -36,13 +37,16 @@ func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
 		velocity += get_gravity() * 1.4 * delta
+		SPEEDY = 3
+	if is_on_floor():
+		SPEEDY = 0
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	var input_dir := Input.get_vector("a", "d", "w", "s")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * (SPEED + SPEEDY)
+		velocity.z = direction.z * (SPEED + SPEEDY)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
--- scripts/tester.gd
+++ scripts/tester.gd
@@ -1,28 +1,31 @@
 extends Node3D
+
 @onready var timer = $Timer
+@onready var timer2 = $Timer2
 @onready var box = preload("res://objects/moveable_box.tscn")
 @onready var sphere = preload("res://objects/moveable_sphere.tscn")
+@onready var world_environment = $WorldEnvironment
+
+var fog_height: float = 1.0  # Starting fog height
+
 func _ready():
 	spawn_obj_b()
 	spawn_obj_s()
 	timer.start()
+	timer2.start()
 
 func spawn_obj_b():
 	var spawnx = randf_range(1, 50)
 	var spawnz = randf_range(-8, 8)
 	var instance = box.instantiate()
-	var new_transform = Transform3D() 
-	new_transform.origin = Vector3(spawnx, 1, spawnz)  
-	instance.global_transform = new_transform
+	instance.global_transform.origin = Vector3(spawnx, 1, spawnz)
 	get_tree().current_scene.add_child(instance)
-	
+
 func spawn_obj_s():
 	var spawnx = randf_range(1, 50)
 	var spawnz = randf_range(-8, 8)
 	var instance = sphere.instantiate()
-	var new_transform = Transform3D()  
-	new_transform.origin = Vector3(spawnx, 1, spawnz)  
-	instance.global_transform = new_transform
+	instance.global_transform.origin = Vector3(spawnx, 1, spawnz)
 	get_tree().current_scene.add_child(instance)
 
 func _on_timer_timeout():
@@ -30,6 +33,8 @@ func _on_timer_timeout():
 	spawn_obj_b()
 	spawn_obj_s()
 
-
-func _on_timer_2_timeout() -> void:
-	pass # Replace with function body.
+func _on_timer2_timeout():
+	timer2.start()
+	fog_height += 0.5
+	var env = world_environment.environment
+	env.fog_height = fog_height
