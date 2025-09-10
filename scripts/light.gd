36d5d26564bd82866909de89ed4f762e06bf9210
PixelizedGames <wf60@mahurangi.school.nz>
2025-09-10 21:07:35 +1200

    alot more optimized

--- scripts/light.gd
+++ scripts/light.gd
@@ -1,33 +1,46 @@
 extends MeshInstance3D
-
 @onready var timer = $Timer
 var light_is_on = true
 
 func _ready():
-	if GlobalVariables.lights:
-		start_random_light_cycle()
-	else:
-		turn_off_light()
+	$SpotLight3D.visible = false
+	$MeshInstance3D.visible = false
+	#if GlobalVariables.lights:
+		#start_random_light_cycle()
+	#else:
+		#turn_off_light()
 
-func start_random_light_cycle():
-	if GlobalVariables.lights:
-		var wait = 0.5
-		if light_is_on:
-			wait = randf_range(1.0, 10.0)
-		timer.start(wait)
+#func start_random_light_cycle():
+	#if GlobalVariables.lights:
+		#var wait = 0.5
+		#if light_is_on:
+			#wait = randf_range(1.0, 10.0)
+		#timer.start(wait)
 
-func _on_timer_timeout():
-	if GlobalVariables.lights:
-		light_is_on = !light_is_on
-		if light_is_on:
-			$SpotLight3D.light_energy = 10.0
-		else:
-			$SpotLight3D.light_energy = 0.0
-		start_random_light_cycle()
-	else:
-		turn_off_light()
+#func _on_timer_timeout():
+	#if GlobalVariables.lights:
+		#light_is_on = !light_is_on
+		#if light_is_on:
+		#	$SpotLight3D.light_energy = 10.0
+		#else:
+			#$SpotLight3D.light_energy = 0.0
+		#start_random_light_cycle()
+	#else:
+		#turn_off_light()
 
-func turn_off_light():
-	timer.stop()
-	light_is_on = false
-	$SpotLight3D.light_energy = 0.0
+#func turn_off_light():
+	#timer.stop()
+	#light_is_on = false
+	#$SpotLight3D.light_energy = 0.0
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is player:
+		$SpotLight3D.visible = true
+		$MeshInstance3D.visible = true
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body is player:
+		if GlobalVariables.lights == true:
+			$SpotLight3D.visible = false
+			$MeshInstance3D.visible = false
